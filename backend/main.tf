90fd2249602d2c5698ed94eb1c83fc6a942f688a
Oussama_Achour <oachour08@gmail.com>
2024-02-25 19:11:31 +0100

    Add : terraform - api gateway

--- backend/main.tf
+++ backend/main.tf
@@ -1,4 +1,32 @@
-# lambda function 
+# ----------------------------lambda function----------------------------# 
+
+resource "aws_lambda_function" "visits-counter" {
+  
+  filename      = "visits-counter.zip"
+  function_name = "visits-counter"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "visits-counter.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.10"
+
+  depends_on = [ aws_cloudwatch_log_group.visits-counter ]
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "visits-counter.py"
+  output_path = "visits-counter.zip"
+}
+
+# Lambda assume role 
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
+}
+
+
 data "aws_iam_policy_document" "lambda_policy" {
   statement {
     sid = "AssumeRole"
@@ -11,70 +39,56 @@ data "aws_iam_policy_document" "lambda_policy" {
     
     actions = ["sts:AssumeRole"]
   }
-#    statement {
-#     sid = "allowAccessToDynamoDB"
-#     effect = "Allow"
-
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com"]
-#     }
-    
-#     actions = [ 
-#         "dynamodb:DeleteItem",
-#         "dynamodb:GetItem",
-#         "dynamodb:PutItem",
-#         "dynamodb:Query",
-#         "dynamodb:Scan",
-#         "dynamodb:UpdateItem"]
-#     resources = [  ]
-#   }
-#   statement {
-#     sid = "allowAccessToCloudWatch"
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com"]  
-#     }
-#     actions = [  
-#         "logs:CreateLogGroup",
-#         "logs:CreateLogStream",
-#         "logs:PutLogEvents" ]
-#     resources = "*"
-#   }
+  }
  
+
+
+
+# lambda integration with cloudwatch : this enables lambda to store logs in cloudwatch
+resource "aws_cloudwatch_log_group" "visits-counter" {
+  name              = "/aws/lambda/visits-counter"
+  retention_in_days = 14
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
+# permissions for accessing cloudwatch and dynamodb
+data "aws_iam_policy_document" "visits-counter" {
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [ 
+         "dynamodb:GetItem",
+         "dynamodb:PutItem",
+         "dynamodb:UpdateItem"
+     ]
+     resources = [ "arn:aws:dynamodb:*:*:table/${aws_dynamodb_table_item.visits-item.table_name}"]
+  }
 }
 
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "visits-counter.py"
-  output_path = "visits-counter.zip"
+resource "aws_iam_policy" "visits-counter" {
+  name        = "visits-counter-logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.visits-counter.json
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "visits-counter.zip"
-  function_name = "visits-counter"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "visits-counter.lambda_handler"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "python3.10"
-
-#   environment {
-#     variables = {
-#       foo = "bar"
-#     }
-#   }
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.visits-counter.arn
 }
 
-# dynamo db 
+# ----------------------------Dynamo DB----------------------------# 
+
 resource "aws_dynamodb_table" "website-visits-table" {
   name             = "website-visits-table"
   hash_key         = var.hash_key
@@ -86,14 +100,6 @@ resource "aws_dynamodb_table" "website-visits-table" {
     name = var.hash_key
     type = "N"
   }
-
-  # replica {
-  #   region_name = "us-east-2"
-  # }
-
-  # replica {
-  #   region_name = "us-west-2"
-  # }
 }
 
 
@@ -115,3 +121,52 @@ resource "aws_dynamodb_table_item" "visits-item" {
   
 }
 
+# ----------------------------api gateway----------------------------# 
+
+# The type of the api is : http   
+ 
+resource "aws_apigatewayv2_api" "visits" {
+  name          = "website-visits-api"
+  protocol_type = "HTTP"
+}
+resource "aws_apigatewayv2_integration" "lambda_get_handler" {
+  api_id = aws_apigatewayv2_api.visits.id
+
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.visits-counter.invoke_arn
+}
+
+resource "aws_apigatewayv2_integration" "lambda_post_handler" {
+  api_id = aws_apigatewayv2_api.visits.id
+
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.visits-counter.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "get_handler" {
+  api_id    = aws_apigatewayv2_api.visits.id
+  route_key = "GET /visits"
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda_get_handler.id}"
+}
+
+resource "aws_apigatewayv2_route" "post_handler" {
+  api_id    = aws_apigatewayv2_api.visits.id
+  route_key = "POST /visits"
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda_post_handler.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visits-counter.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.visits.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "visits" {
+  api_id = aws_apigatewayv2_api.visits.id
+  name   = "prod"
+}
