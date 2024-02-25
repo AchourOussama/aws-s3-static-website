a9fa844135856f4f5d4896693a21c5c9f5f457aa
Oussama_Achour <oachour08@gmail.com>
2024-02-25 19:10:56 +0100

    Add : Lambda function code

--- backend/visits-counter.py
+++ backend/visits-counter.py
@@ -2,53 +2,60 @@ import json
 import boto3
 
 
-# dynamodb = boto3.resource('dynamodb')
-# table = dynamodb.Table('personal-website-visits')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('website-visits-table')
 
 def lambda_handler(event, context):
-    print("hello world")
+    # print("hello world")
     # return "hello"
-    # http_method = event['httpMethod']
+    # return {
+    #     'statusCode': 200,
+    #     'headers': {
+    #         'Content-Type': 'application/json',
+    #     },
+    #     'body': 'Hello, World!',
+    # }
+    http_method = event['httpMethod']
 
-    # if http_method == 'POST':
-    #     response = table.get_item(Key={'id':'0'})
-    #     count = response['Item']['count']
-    #     count = count + 1
-    #     print(count)
-    #     response = table.put_item(Item={'id':'0', 'count': count})
+    if http_method == 'POST':
+        response = table.get_item(Key={'id': 0})
+        count = response['Item']['count']
+        count = count + 1
+        print(count)
+        response = table.put_item(Item={'id':0, 'count': count})
 
-    #     return {
-    #         "statusCode": 200,
-    #         "headers": {
-    #             "Content-Type": "application/json",
-    #             "Access-Control-Allow-Origin": "*",
-    #             "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
-    #         },
-    #         "body": json.dumps({
-    #             "message": f"New view count: {count}"
-    #         })
-    #     }
-    # elif http_method == 'GET':
-    #     response = table.get_item(Key={'id':'0'})
-    #     count = response['Item']['count']
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+            },
+            "body": json.dumps({
+                "message": f"New view count: {count}"
+            })
+        }
+    elif http_method == 'GET':
+        response = table.get_item(Key={'id': 0})
+        count = response['Item']['count']
     
-    #     return {
-    #         "statusCode": 200,
-    #         "headers": {
-    #             "Access-Control-Allow-Origin": "*",
-    #             "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-    #             "Access-Control-Allow-Methods": "OPTIONS,GET",
-    #             "Content-Type": "application/json"
-    #         },
-    #         "body": json.dumps({"count is": str(count)})
-    #     }
-    # else:
-    #     return {
-    #         "statusCode": 405,
-    #         "headers": {
-    #             "Content-Type": "application/json",
-    #             "Access-Control-Allow-Origin": "*",
-    #             "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
-    #         },
-    #         "body": json.dumps({"error": "Method not allowed"})
-    #     }
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods": "OPTIONS,GET",
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"count is": str(count)})
+        }
+    else:
+        return {
+            "statusCode": 405,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+            },
+            "body": json.dumps({"error": "Method not allowed"})
+        }
