451cbd965c0f1b8840ae58cdecb485d8f41e86e3
Oussama_Achour <oachour08@gmail.com>
2024-02-27 00:11:00 +0100

    Add: personal website with the visits counter feature

--- frontend/personal-website/test.js
+++ frontend/personal-website/test.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+
+
+
+
+// Check if the 'count' element exists
+const countElement = document.getElementById('count');
+
+if (countElement) {
+  // 'count' element exists
+  console.log('Element with ID "count" exists.');
+} else {
+  // 'count' element does not exist
+  console.error('Element with ID "count" does not exist.');
+}
--- frontend/personal-website/visitors.js
+++ frontend/personal-website/visitors.js
@@ -1,23 +1,69 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("hello before function ") ; 
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log("hello before function ") ; 
+//     const apiUrl = 'https://3a04q82le8.execute-api.us-east-1.amazonaws.com/prod/visits'
+//     // POST method to increment the visitor count
+    
+//     // GET method to retrieve the current visitor count and display it on the webpage
+//     async function getCurrentCount() {
+ 
+//     // testing 
+//       const count = 100; 
+//       const countElement = document.getElementById('count');
+//       countElement.innerText = "count";
+//       const greeting = document.getElementById('hello');
+//       greeting.innerText = "hello world";
+      
+//     }
+  
+//     getCurrentCount();
+//   });
+
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   console.log("hello before function ") ; 
+  //   const apiUrl = 'https://3a04q82le8.execute-api.us-east-1.amazonaws.com/prod/visits'
+  //   // Example POST request
+  //   fetch(`${apiUrl}`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       // Add any additional headers if required
+  //     },
+  //     // Add the request body for POST requests
+  //     body: JSON.stringify({}),
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log('Response from API:', data);
+  //       // Process the data returned by the API
+  //       // You might want to display the count on your webpage
+  //       const countElement = document.getElementById('count');
+  //       countElement.innerText = `Count: ${data.count}`;
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //       // Handle errors
+  //     });
+  // });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log("from script")
+   
     // POST method to increment the visitor count
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('POST', 'https://yk56eavo5f.execute-api.us-east-1.amazonaws.com/prod/count');
-    // xhr.send();
+    const apiUrl = 'https://sfanvsifba.execute-api.us-east-1.amazonaws.com/prod/visits'
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', apiUrl);
+    xhr.send();
   
     // GET method to retrieve the current visitor count and display it on the webpage
     async function getCurrentCount() {
-    //   const response = await fetch('https://yk56eavo5f.execute-api.us-east-1.amazonaws.com/prod/count');
-    //   const data = await response.json();
-    //   const count = data.count;
-    
-    // testing 
-      const count = 100; 
-      const countElement = document.getElementById('count');
-      countElement.innerText = "count";
-      const greeting = document.getElementById('hello');
-      greeting.innerText = "hello world";
-      
+      const response = await fetch(apiUrl );
+      const data = await response.json();
+      console.log(data);
+      const count = data["count is"];
+      var countElement = document.getElementById('count');
+      //countElement.innerText = count;
+      countElement.innerText =`Count: ${count}`;
     }
   
     getCurrentCount();
