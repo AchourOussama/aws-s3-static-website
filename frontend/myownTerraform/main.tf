3ae12f4d228cf9a6719a12a86e873950707e8be5
Oussama_Achour <oachour08@gmail.com>
2024-04-04 10:39:36 +0100

    Update: making s3 private

--- frontend/myownTerraform/main.tf
+++ frontend/myownTerraform/main.tf
@@ -43,8 +43,8 @@ resource "aws_s3_bucket_public_access_block" "website" {
   bucket                  = aws_s3_bucket.website.id
   #ignore_public_acls      = true
   #restrict_public_buckets = true
-  block_public_acls       = false
-  block_public_policy     = false
+  block_public_acls       = true
+  block_public_policy     = true
 }
 
 # bucket policy 
